5bb59a0d0caa5d57d3450f1ace184bc8f2088aa1
jtpark <vigabhan1231@gmail.com>
2022-03-13 18:38:11 +0900

    chore: git hook을 위한 세팅 추가
    git commit시 필요한 lint 설정들을 추가함
    - stylelint, commitlint(husky)
    - twin.macro를 위한 type 추가

--- types/twin.d.ts
+++ types/twin.d.ts
@@ -8,3 +8,24 @@ declare module 'twin.macro' {
   const styled: typeof styledImport;
   const css: typeof cssImport;
 }
+
+declare module 'react' {
+  // The css prop
+  interface HTMLAttributes<T> extends DOMAttributes<T> {
+    css?: CSSProp;
+  }
+  // The inline svg css prop
+  interface SVGProps<T>
+    extends SVGProps<T extends SVGSVGElement ? T : unknown> {
+    css?: CSSProp;
+  }
+}
+
+// The 'as' prop on styled components
+declare global {
+  namespace JSX {
+    interface IntrinsicAttributes<T> extends DOMAttributes<T> {
+      as?: string;
+    }
+  }
+}
